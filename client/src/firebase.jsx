ca18655536b7f28a14cd02bb49825c2455746659
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-06-29 15:26:52 +0300

    firebase auth

--- client/src/firebase.jsx
+++ client/src/firebase.jsx
@@ -4,15 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_firebase_API_KEY,
-  authDomain: "pata-keja-ke.firebaseapp.com",
-  projectId: "pata-keja-ke",
-  storageBucket: "pata-keja-ke.appspot.com",
-  messagingSenderId: "382144798886",
-  appId: "1:382144798886:web:c06922c8f329fd30ad5467",
-  measurementId: "G-5XPKHL08KW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "pata-keja-5d086.firebaseapp.com",
+  projectId: "pata-keja-5d086",
+  storageBucket: "pata-keja-5d086.appspot.com",
+  messagingSenderId: "94078031677",
+  appId: "1:94078031677:web:f5d3467bdfefad7422d754"
 };
 
 // Initialize Firebase
