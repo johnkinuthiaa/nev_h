f129122a08b1882ddc731119a791a9d17ffcecaa
John Kinuthia <156569773+johnkinuthiaa@users.noreply.github.com>
2024-06-29 13:27:52 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ import path from 'path';
 dotenv.config();
 
 mongoose
-  .connect('mongodb+srv://kinuthiajohn122:lightyagami@pata-keja.vyxrdxp.mongodb.net/?retryWrites=true&w=majority&appName=pata-keja')
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
   })
