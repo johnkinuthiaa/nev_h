ad6752310bb8b1a1655ca440c0fc0fe4987f0e6f
John Kinuthia <156569773+johnkinuthiaa@users.noreply.github.com>
2024-06-30 09:23:51 +0300

    Update firebase.jsx

--- client/src/firebase.jsx
+++ client/src/firebase.jsx
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDWLqI4cC1l-2Rttvh1hfA4hBHhkiLucFE",
   authDomain: "pata-keja-a1d60.firebaseapp.com",
