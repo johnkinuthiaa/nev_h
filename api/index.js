8fbcdcb2d95801189c0b8dadbb461efffff89d8d
John Kinuthia <156569773+johnkinuthiaa@users.noreply.github.com>
2024-07-01 08:44:13 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose
     console.log(err);
   });
 
-  const path = require('path');
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -34,10 +34,10 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(path.resolve(), '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.resolve(), 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
@@ -49,4 +49,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-
