032834e08eeb83ec9b7172a512014db012e062be
John Kinuthia <156569773+johnkinuthiaa@users.noreply.github.com>
2024-07-06 10:49:33 +0300

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -16,7 +16,7 @@ export default function About() {
       </p>
       <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
     </div>
-    </div
+    </div>
    
   )
 }
